f5afdc2c28defbc29932a9e014136add788b1b27
Robin Persson <robinpn98@hotmail.se>
2023-04-12 10:41:45 +0200

    added title

--- sw-char-creator/src/App.js
+++ sw-char-creator/src/App.js
@@ -4,10 +4,11 @@ import backgroundImage from "./images/background-image.png";
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    ></div>
+    <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="title">
+        <h1 style={{ color: "white" }}>Star Wars Character Generator</h1>
+      </div>
+    </div>
   );
 }
 
