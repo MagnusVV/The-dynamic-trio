6b476cd81d7952f499f8564cc6bc9f8dcdabc016
Robin Persson <robinpn98@hotmail.se>
2023-04-12 09:55:39 +0200

    added background image

--- sw-char-creator/src/App.js
+++ sw-char-creator/src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import backgroundImage from "./images/background-image.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div
+      className="App"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    ></div>
+  );
 }
 
 export default App;
