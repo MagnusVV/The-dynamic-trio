ee1b429000facbf9e0e6ac535da7028ab455157f
Robin Persson <robinpn98@hotmail.se>
2023-04-19 14:03:36 +0200

    added focus styling to input

--- sw-char-creator/src/components/Input/Input.js
+++ sw-char-creator/src/components/Input/Input.js
@@ -5,6 +5,12 @@ const Input = styled.input`
   width: 10rem;
   padding: 8px 20px;
   border-radius: 20px;
+  background-color: black;
+  color: white;
+  &:focus {
+    outline: none;
+    border: 2px solid #2ff923;
+  }
 `;
 
 export default Input;
